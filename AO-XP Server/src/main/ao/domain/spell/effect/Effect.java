f34846fbdd01f2c3e56176616c852649336eddb2
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2009-08-20 17:21:29 +0000

    Armé la Interfaz Effect
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@11 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/domain/spell/effect/Effect.java
+++ AO-XP Server/src/main/ao/domain/spell/effect/Effect.java
@@ -1,5 +1,13 @@
 package ao.domain.spell.effect;
 
+import ao.domain.character.Character;
+
 public interface Effect {
+
+	/**
+	 * Applies the effect in the given character
+	 * @param character The character to apply the effect.
+	 */
+	void apply(Character character);
 
 }
