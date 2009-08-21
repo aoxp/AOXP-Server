62d7c315ebfca10388869900fd98e9cdddfb24b7
Samuel Fernandez <zaxtor@gmail.com>
2009-08-21 04:28:45 +0000

    Algunos metodos para movementStrategy
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@16 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/domain/character/movement/MovementStrategy.java
+++ AO-XP Server/src/main/ao/domain/character/movement/MovementStrategy.java
@@ -2,4 +2,14 @@ package ao.domain.character.movement;
 
 public interface MovementStrategy {
 
+	/**
+	 * Applies the movement strategy
+	 */
+	void move();
+	
+	/**
+	 * Targets a new character
+	 * @param target	The character to target
+	 */
+	void setTarget(Character target);
 }
