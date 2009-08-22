dcfa94027475661e842e28a20fa0a8e83bc60ab3
Franco Zeoli <zeolifranco@gmail.com>
2009-08-22 20:01:25 +0000

    Interfaz Reputation.
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@34 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/domain/character/Reputation.java
+++ AO-XP Server/src/main/ao/domain/character/Reputation.java
@@ -1,6 +1,66 @@
 package ao.domain.character;
 
 public interface Reputation {
-
+	
 	Alignment getAlignment();
+	
+	/**
+	 * Retrieves the bandit points.
+	 * @return The reputation's bandit points.
+	 */
+	int getBandit();
+	
+	/**
+	 * Adds the given points to the reputation's bandit points.
+	 * @param points The points to add.
+	 */
+	void addToBandit(int points);
+	
+	/**
+	 * Retrieves the assasin points.
+	 * @return The reputation's assasin points.
+	 */
+	int getAssasin();
+	
+	/**
+	 * Adds the given poitns to the reputation's assasin points.
+	 * @param points The points to add.
+	 */
+	void addToAssasin(int points);
+	
+	/**
+	 * Retrieves the noble points.
+	 * @return The reputation's noble points.
+	 */
+	int getNoble();
+	
+	/**
+	 * Adds the given points to the reputation's noble points.
+	 * @param points The points to add.
+	 */
+	void addToNoblePoints(int points);
+	
+	/**
+	 * Retrieves the bourgeois points.
+	 * @return The reputation's bourgeois points.
+	 */
+	int getBourgeois();
+	
+	/**
+	 * Adds the given points to the reputation's bourgeois points.
+	 * @param points The points to add.
+	 */
+	void addToBourgeois(int points);
+	
+	/**
+	 * Retrieves the thieff points.
+	 * @return The thieff reputation's points.
+	 */
+	int getThieff();
+	
+	/**
+	 * Adds the given poitns to the reputation's thieff points.
+	 * @param points The points to add.
+	 */
+	void addToThieff(int points);
 }
