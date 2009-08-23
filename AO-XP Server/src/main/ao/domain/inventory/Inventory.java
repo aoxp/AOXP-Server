d81fb1274cda8f4254ba8c48dfce71dae4257319
Marco Vanotti <marcovanotti15@gmail.com>
2009-08-23 01:36:12 +0000

    Implementacion de la clase inventario.
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@37 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/domain/inventory/Inventory.java
+++ AO-XP Server/src/main/ao/domain/inventory/Inventory.java
@@ -19,10 +19,10 @@ public interface Inventory {
 	/**
 	 * Adds an Item to the inventory.
 	 * @param item references to the item to add
-	 * @return True if the item was successfully added (the full amount).
-	 * 		If not, the amount is altered to match the remaining.
+	 * @return 0	if the item was successfully added or
+	 * 				the amount of the item that could not be added
 	 */
-	boolean addItem(Item item);
+	int addItem(Item item);
 	
 	/**
 	 * Removes the item of the desired slot.
@@ -60,5 +60,18 @@ public interface Inventory {
 	 * @return the slot of the item in the inventory or -1 if the item isn't in the inventory
 	 */
 	int hasItem(Item item);
-
+	
+	/**
+	 * Gets the current inventory capacity
+	 * @return the number of slots in the inventory
+	 */
+	int getCapacity();
+	
+	
+	/**
+	 * Gets the amount of an item  in the inventory
+	 * @param item to look for its amount
+	 * @return	the amount of the item.
+	 */
+	int getItemAmount(Item item);
 }
--- AO-XP Server/src/main/ao/domain/inventory/InventoryImpl.java
+++ AO-XP Server/src/main/ao/domain/inventory/InventoryImpl.java
@@ -3,55 +3,161 @@ package ao.domain.inventory;
 import ao.domain.worldobject.Item;
 
 public class InventoryImpl implements Inventory {
-	// TODO : Esto está cableado maaaal!!
-	Item[] inventory = new Item[18];
+	
+	Item[] inventory;
+	
+	public InventoryImpl(){
+		this(18);
+	}
+	
+	public InventoryImpl(int slots){
+		if (slots <= 0){
+			return;
+		}
+		
+		inventory = new Item[slots];
+	}
 	
 	@Override
-	public boolean addItem(Item item) {
-		// TODO Auto-generated method stub
-		return false;
+	public int addItem(Item item) {
+		if (this.hasItem(item) != -1){
+			int amount = item.getAmount();
+			
+			for (int i = 0; i < inventory.length; i++){
+				if (inventory[i].getId() == item.getId()){
+					amount = inventory[i].addAmount(amount); 
+					if (amount == 0){
+						return 0;
+					}
+				}
+			}
+			item.addAmount(-(item.getAmount() - amount));
+		}
+		
+		for (int i = 0; i < inventory.length; i++){
+			if (inventory[i] == null){
+				inventory[i] = item;
+				return 0;
+			}
+		}
+		
+		return item.getAmount();
 	}
 
 	@Override
 	public Item getItem(int slot) {
-		// TODO Auto-generated method stub
-		return null;
+		if (slot < 0 || slot >= inventory.length){
+			return null;	
+		}
+		
+		return inventory[slot];
 	}
 
 	@Override
 	public boolean hasFreeSlots() {
 		// TODO Auto-generated method stub
-		for (Item item : inventory) {
-			if (item == null ) {
+		for (Item item : inventory){
+			if (item == null ){
 				return true;
-			}
+			}		
 		}
-		
+			
 		return false;
 	}
 
 	@Override
 	public int hasItem(Item item) {
-		// TODO Auto-generated method stub
-		return 0;
+		for (int i = 0; i < inventory.length; i++){
+			if (inventory[i].getId() == item.getId()){
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 
 	@Override
 	public Item removeItem(int slot) {
-		// TODO Auto-generated method stub
-		return null;
+		Item item = getItem(slot);
+		if (item == null){
+			return null;
+		}
+
+		inventory[slot] = null;
+		return item;
 	}
 
 	@Override
 	public Item removeItem(Item item) {
-		// TODO Auto-generated method stub
-		return null;
+		//TODO: Empezar a usar la parte lo que devuelve addAmount.
+		if ( this.hasItem(item) == -1 ){
+			return null;
+		}
+		
+		Item itemRemoved = item.clone();
+		int remainingAmount = item.getAmount();
+		
+		for (int i = 0; i < inventory.length; i++){
+			if (inventory[i].getId() == item.getId()) {
+				remainingAmount -= inventory[i].getAmount();
+				inventory[i].addAmount( -(inventory[i].getAmount()+remainingAmount));
+				
+				if (inventory[i].getAmount() <= 0){
+					inventory[i] = null;
+				}
+				
+				if (remainingAmount <= 0){
+					return itemRemoved;
+				}
+			}
+		}
+		
+		itemRemoved.addAmount(-remainingAmount);
+		
+		return itemRemoved;
 	}
 
 	@Override
 	public Item removeItem(int slot, int amount) {
-		// TODO Auto-generated method stub
-		return null;
+		Item item = getItem(slot);
+		if (item == null){
+			return null;
+		}
+
+		Item itemRemoved = item;
+		
+		item.addAmount(-amount);
+		
+		/**
+		 * Remove the remaining amount from a copy of the item,
+		 * so the copy will end with the amount of removed items
+		 */
+		itemRemoved.addAmount(-item.getAmount());
+		
+		if (item.getAmount() <= 0){
+			inventory[slot] = null;
+		}
+
+		return itemRemoved;
+	}
+
+	@Override
+	public int getCapacity() {
+		return inventory.length;
+	}
+
+	@Override
+	public int getItemAmount(Item item) {
+
+		int amount = 0;
+		
+		for (Item i : inventory){
+			if (item.getId() == i.getId()){
+				amount += i.getAmount();
+			}
+		}
+		
+		return amount;
 	}
 
 }
