8bb407e66c1fb2e284ba628bbf74ae3f8ee5289b
Samuel Fernandez <zaxtor@gmail.com>
2009-08-26 02:23:13 +0000

    LumberjackArchetype added
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@48 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

---
+++ AO-XP Server/src/main/ao/domain/character/archetype/LumberjackArchetype.java
@@ -0,0 +1,57 @@
+package ao.domain.character.archetype;
+
+/**
+ * A lumberjack archetype.
+ * @author Zama
+ */
+public class LumberjackArchetype {
+
+	private static final int HIT_INCREMENT = 2;
+	private static final int STAMINA_INCREMENT = 35;
+	private static final int LUMBER_MIN_AMOUNT = 1;
+	private static final int LUMBER_MAX_AMOUNT = 4;
+	private static final int LUMBER_STAMINA_COST = 2;
+
+	/**
+	 * Retrieves the hit increment.
+	 * @param level The number of levels raised.
+	 * @return The hit increment.
+	 */
+	public int getHitIncrement(int level) {
+		return HIT_INCREMENT;
+	}
+
+	/**
+	 * Retrieves the stamina increment for the archetype.
+	 * @param level The number of levels raised.
+	 * @return The stamina increment for the archetype.
+	 */
+	public int getStaminaIncrement(int level) {
+		return STAMINA_INCREMENT;
+	}
+		
+	/**
+	 * Retrieves the maximum amount that can be Lumbered.
+	 * @return The maximum amount that can be Lumbered.
+	 */
+	public int getLumberedMaxAmount() {
+		return LUMBER_MAX_AMOUNT;
+	}
+
+	/**
+	 * Retrieves the minimum amount that can be Lumbered.
+	 * @return The minimum amount that can be Lumbered.
+	 */
+	public int getLumberedMinAmount() {
+		return LUMBER_MIN_AMOUNT;
+	}
+	
+	/**
+	 * Retrieves the needed amount of stamina to Lumber.
+	 * @return The needed amount of stamina to Lumber.
+	 */
+	public int getLumberjackingStaminaCost(){
+		return LUMBER_STAMINA_COST;
+	}
+	
+}
