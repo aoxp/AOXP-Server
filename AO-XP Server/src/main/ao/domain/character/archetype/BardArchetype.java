4d28f5de25d8ac1a0ad6a92f1d9c64488618a0f8
Brian Chaia <brian.pr@gmail.com>
2009-08-27 15:13:55 +0000

    Arreglo manqueada de pisar métodos
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@56 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/domain/character/archetype/BardArchetype.java
+++ AO-XP Server/src/main/ao/domain/character/archetype/BardArchetype.java
@@ -7,21 +7,9 @@ package ao.domain.character.archetype;
 public class BardArchetype extends DefaultArchetype {
 
 	private static final float MANA_MODIFIER = 2.0f;
-	private static final int HIT_INCREMENT = 2;
-	private static final int STAMINA_INCREMENT = 15;
 	
 	@Override
 	public int getManaIncrement(int intelligence, int mana) {
 		return (int) Math.round(intelligence * MANA_MODIFIER);
-	}
-	
-	@Override
-	public int getHitIncrement(int level) {
-		return HIT_INCREMENT;
-	}
-	
-	@Override
-	public int getStaminaIncrement() {
-		return STAMINA_INCREMENT;
 	}
 }
