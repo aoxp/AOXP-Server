959118ae58246e052b759601bba3efd5de03cb59
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2009-08-28 22:28:54 +0000

    5 minutos libres == algo de sockets codeado. Sigan ignorandolo, ya lo voy a completar....
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@60 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/AOXPServer.java
+++ AO-XP Server/src/main/ao/AOXPServer.java
@@ -1,5 +1,11 @@
 package ao;
 
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -9,13 +15,38 @@ import java.util.concurrent.ExecutorService;
 public class AOXPServer {
 
 	private ExecutorService threadPool;
+	private ServerSocketChannel ssc;
+	private Selector selector;
 
 	/**
 	 * Starts running the AOXPServer. It MUST be properly initialized beforehand.
 	 */
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		try {
+			// TODO This should really end when the app is somehow interrupted...
+			while (true) {
+				// Wait for any activity (either an incoming connection or incoming data on an existing connection)
+				int num = selector.select();
+				
+				if (num == 0) {
+					continue;	// No activity, continue
+				}
+				
+				// Process detected events
+				Set keys = selector.selectedKeys();
+				Iterator it = keys.iterator();
+				while (it.hasNext()) {
+					SelectionKey key = (SelectionKey)it.next();
+					
+					// TODO : deal with SelectionKey
+				}
+				
+				keys.clear();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -24,6 +55,22 @@ public class AOXPServer {
 	 */
 	public void setThreadPool(ExecutorService threadPool) {
 		this.threadPool = threadPool;
+	}
+
+	/**
+	 * Sets the server's socket channel.
+	 * @param ssc The channel to be used by the server.
+	 * @throws IOException
+	 */
+	public void setServerSocketChannel(ServerSocketChannel ssc) throws IOException {
+		// Reset selector
+		this.selector = Selector.open();
+		
+		this.ssc = ssc;
+		
+		// Set general configuration
+		this.ssc.configureBlocking(false);
+		this.ssc.register(selector, SelectionKey.OP_ACCEPT);
 	}
 
 }
