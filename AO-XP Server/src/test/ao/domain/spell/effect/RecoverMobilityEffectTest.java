b04f320dc3249e179e256b195dab632569c3917e
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2009-09-01 20:39:42 +0000

    Otro test mas, cpon este hasta encontre un bug de implementacion qeu de paso arregle. Ven que testear sirve? :P
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@72 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

---
+++ AO-XP Server/src/test/ao/domain/spell/effect/RecoverMobilityEffectTest.java
@@ -0,0 +1,97 @@
+package ao.domain.spell.effect;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ao.domain.character.Character;
+import ao.domain.worldobject.WorldObject;
+
+public class RecoverMobilityEffectTest {
+
+	private Effect recoverMobilityEffect;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.recoverMobilityEffect = new RecoverMobilityEffect();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testApplyCharacterCharacter() {
+		Character caster = EasyMock.createMock(Character.class);
+		Character target = EasyMock.createMock(Character.class);
+		
+		target.setImmobilized(false);
+		target.setParalyzed(false);
+		
+		EasyMock.replay(caster, target);
+		
+		recoverMobilityEffect.apply(caster, target);
+		
+		EasyMock.verify(caster, target);
+	}
+
+	@Test
+	public void testAppliesToCharacterCharacter() {
+		Character caster = EasyMock.createMock(Character.class);
+		Character target = EasyMock.createMock(Character.class);
+		
+		// Pretend the target is immobilized first, then just paralyzed, finally none.
+		EasyMock.expect(target.isImmobilized()).andReturn(true).once();
+		EasyMock.expect(target.isDead()).andReturn(false).once();
+		target.setParalyzed(false);
+		target.setImmobilized(false);
+		
+		EasyMock.expect(target.isImmobilized()).andReturn(false).once();
+		EasyMock.expect(target.isParalyzed()).andReturn(true).once();
+		EasyMock.expect(target.isDead()).andReturn(false).once();
+		target.setParalyzed(false);
+		target.setImmobilized(false);
+		
+		EasyMock.expect(target.isImmobilized()).andReturn(false).once();
+		EasyMock.expect(target.isParalyzed()).andReturn(false).once();
+		
+		EasyMock.replay(caster, target);
+		
+		// An immobilized char is valid.
+		Assert.assertTrue(recoverMobilityEffect.appliesTo(caster, target));
+		
+		// A paralyzed char is valid.
+		Assert.assertTrue(recoverMobilityEffect.appliesTo(caster, target));
+		
+		// A non-paralyzed, non/immobilized char is invalid.
+		Assert.assertFalse(recoverMobilityEffect.appliesTo(caster, target));
+	}
+
+	@Test
+	public void testAppliesToCharacterWorldObject() {
+		WorldObject obj = EasyMock.createMock(WorldObject.class);
+		Character caster = EasyMock.createMock(Character.class);
+		
+		EasyMock.replay(obj, caster);
+		
+		// Should always false, no matter what
+		Assert.assertFalse(recoverMobilityEffect.appliesTo(caster, obj));
+	}
+
+	@Test
+	public void testApplyCharacterWorldObject() {
+		WorldObject obj = EasyMock.createMock(WorldObject.class);
+		Character caster = EasyMock.createMock(Character.class);
+		
+		EasyMock.replay(obj, caster);
+		
+		// Should do nothing....
+		recoverMobilityEffect.apply(caster, obj);
+		
+		EasyMock.verify(caster, obj);
+	}
+
+}
