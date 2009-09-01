0d10dff7707fa56fb0cf99404266ce915d243e3c
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2009-09-01 20:44:46 +0000

    Otro test mas!
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@73 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

---
+++ AO-XP Server/src/test/ao/domain/spell/effect/ImmobilizationEffectTest.java
@@ -0,0 +1,83 @@
+package ao.domain.spell.effect;
+
+import static org.junit.Assert.*;
+
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import ao.domain.character.Character;
+import ao.domain.worldobject.WorldObject;
+
+public class ImmobilizationEffectTest {
+
+	private Effect immobilizationEffect;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.immobilizationEffect = new ImmobilizationEffect();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testApplyCharacterCharacter() {
+		Character caster = EasyMock.createMock(Character.class);
+		Character target = EasyMock.createMock(Character.class);
+		
+		target.setImmobilized(true);
+		
+		EasyMock.replay(caster, target);
+		
+		immobilizationEffect.apply(caster, target);
+		
+		EasyMock.verify(caster, target);
+	}
+
+	@Test
+	public void testAppliesToCharacterCharacter() {
+		Character caster = EasyMock.createMock(Character.class);
+		Character target = EasyMock.createMock(Character.class);
+		
+		// Pretend the target is dead first, then alive.
+		EasyMock.expect(target.isDead()).andReturn(true).once();
+		EasyMock.expect(target.isDead()).andReturn(false).once();
+		
+		EasyMock.replay(caster, target);
+		
+		// Paralyzing a dead char is invalid.
+		Assert.assertFalse(immobilizationEffect.appliesTo(caster, target));
+		
+		/// Paralyzing an alive char is valid.
+		Assert.assertTrue(immobilizationEffect.appliesTo(caster, target));
+	}
+
+	@Test
+	public void testAppliesToCharacterWorldObject() {
+		WorldObject obj = EasyMock.createMock(WorldObject.class);
+		Character caster = EasyMock.createMock(Character.class);
+		
+		EasyMock.replay(obj, caster);
+		
+		// Should always false, no matter what
+		Assert.assertFalse(immobilizationEffect.appliesTo(caster, obj));
+	}
+
+	@Test
+	public void testApplyCharacterWorldObject() {
+		WorldObject obj = EasyMock.createMock(WorldObject.class);
+		Character caster = EasyMock.createMock(Character.class);
+		
+		EasyMock.replay(obj, caster);
+		
+		// Should do nothing....
+		immobilizationEffect.apply(caster, obj);
+		
+		EasyMock.verify(caster, obj);
+	}
+
+}
