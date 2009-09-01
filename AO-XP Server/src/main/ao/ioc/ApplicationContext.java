d69d42ab4f07be62135bdd8317696183fa99e58e
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2009-09-01 20:21:06 +0000

    Hago un test para Fran y arreglo una boludez. Me cronometre, 3:20 me tomo hacer el test completo. Fran, no llores con EasyMock, es un gol de media cancha.
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@69 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/ioc/ApplicationContext.java
+++ AO-XP Server/src/main/ao/ioc/ApplicationContext.java
@@ -1,8 +1,6 @@
 package ao.ioc;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
---
+++ AO-XP Server/src/test/ao/domain/spell/effect/InvisibilityEffectTest.java
@@ -0,0 +1,85 @@
+package ao.domain.spell.effect;
+
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import ao.domain.character.Character;
+import ao.domain.character.NPCCharacter;
+import ao.domain.character.UserCharacter;
+import ao.domain.worldobject.WorldObject;
+
+public class InvisibilityEffectTest {
+
+	private Effect invisibilityEffect;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.invisibilityEffect = new InvisibilityEffect();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testApplyCharacterCharacter() {
+		Character caster = EasyMock.createMock(Character.class);
+		Character target = EasyMock.createMock(Character.class);
+		target.setInvisible(true);
+		
+		EasyMock.replay(caster, target);
+		
+		invisibilityEffect.apply(caster, target);
+		
+		EasyMock.verify(caster, target);
+	}
+
+	@Test
+	public void testAppliesToCharacterCharacter() {
+		Character caster = EasyMock.createMock(Character.class);
+		UserCharacter userTarget = EasyMock.createMock(UserCharacter.class);
+		NPCCharacter target = EasyMock.createMock(NPCCharacter.class);
+		
+		// First fake it's dead, then pretend it's alive.
+		EasyMock.expect(userTarget.isDead()).andReturn(true).once();
+		EasyMock.expect(userTarget.isDead()).andReturn(false).once();
+		
+		EasyMock.replay(caster, target, userTarget);
+		
+		// Test invalid target
+		Assert.assertFalse(invisibilityEffect.appliesTo(caster, target));
+		
+		// Test dead target
+		Assert.assertFalse(invisibilityEffect.appliesTo(caster, userTarget));
+		
+		// Test alive target
+		Assert.assertTrue(invisibilityEffect.appliesTo(caster, userTarget));
+	}
+
+	@Test
+	public void testAppliesToCharacterWorldObject() {
+		WorldObject obj = EasyMock.createMock(WorldObject.class);
+		Character caster = EasyMock.createMock(Character.class);
+		
+		EasyMock.replay(obj, caster);
+		
+		Assert.assertFalse(invisibilityEffect.appliesTo(caster, obj));
+	}
+
+	@Test
+	public void testApplyCharacterWorldObject() {
+		WorldObject obj = EasyMock.createMock(WorldObject.class);
+		Character caster = EasyMock.createMock(Character.class);
+		
+		EasyMock.replay(obj, caster);
+		
+		// This should do nothing
+		invisibilityEffect.apply(caster, obj);
+		
+		EasyMock.verify(caster, obj);
+	}
+
+}
