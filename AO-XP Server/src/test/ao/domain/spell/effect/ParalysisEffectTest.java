47628c65d6931ad5bc5847cd19ca2f34e555d45b
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2009-09-01 20:28:50 +0000

    Salen como chorizo.....
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@70 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

---
+++ AO-XP Server/src/test/ao/domain/spell/effect/ParalysisEffectTest.java
@@ -0,0 +1,82 @@
+package ao.domain.spell.effect;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ao.domain.character.Character;
+import ao.domain.worldobject.WorldObject;
+
+public class ParalysisEffectTest {
+
+	private Effect paralysisEffect;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.paralysisEffect = new ParalysisEffect();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testApplyCharacterCharacter() {
+		Character caster = EasyMock.createMock(Character.class);
+		Character target = EasyMock.createMock(Character.class);
+		
+		target.setParalyzed(true);
+		
+		EasyMock.replay(caster, target);
+		
+		paralysisEffect.apply(caster, target);
+		
+		EasyMock.verify(caster, target);
+	}
+
+	@Test
+	public void testAppliesToCharacterCharacter() {
+		Character caster = EasyMock.createMock(Character.class);
+		Character target = EasyMock.createMock(Character.class);
+		
+		// Pretend the target is dead first, then alive.
+		EasyMock.expect(target.isDead()).andReturn(true).once();
+		EasyMock.expect(target.isDead()).andReturn(false).once();
+		
+		EasyMock.replay(caster, target);
+		
+		// Paralyzing a dead char is invalid.
+		Assert.assertFalse(paralysisEffect.appliesTo(caster, target));
+		
+		/// Paralyzing an alive char is valid.
+		Assert.assertTrue(paralysisEffect.appliesTo(caster, target));
+	}
+
+	@Test
+	public void testAppliesToCharacterWorldObject() {
+		WorldObject obj = EasyMock.createMock(WorldObject.class);
+		Character caster = EasyMock.createMock(Character.class);
+		
+		EasyMock.replay(obj, caster);
+		
+		// Should always false, no matter what
+		Assert.assertFalse(paralysisEffect.appliesTo(caster, obj));
+	}
+
+	@Test
+	public void testApplyCharacterWorldObject() {
+		WorldObject obj = EasyMock.createMock(WorldObject.class);
+		Character caster = EasyMock.createMock(Character.class);
+		
+		EasyMock.replay(obj, caster);
+		
+		// Should do nothing....
+		paralysisEffect.apply(caster, obj);
+		
+		EasyMock.verify(caster, obj);
+	}
+
+}
