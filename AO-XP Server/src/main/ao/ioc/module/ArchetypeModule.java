b7f322e86196657bdd7ebc46623bb58c4c0e171b
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2009-10-14 01:55:13 +0000

    Agrego los providers de archetypes que faltaban.
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@104 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/ioc/module/ArchetypeModule.java
+++ AO-XP Server/src/main/ao/ioc/module/ArchetypeModule.java
@@ -2,14 +2,22 @@ package ao.ioc.module;
 
 import ao.config.ArchetypeConfiguration;
 import ao.model.character.archetype.Archetype;
+import ao.model.character.archetype.AssasinArchetype;
+import ao.model.character.archetype.BanditArchetype;
 import ao.model.character.archetype.BardArchetype;
+import ao.model.character.archetype.BlacksmithArchetype;
+import ao.model.character.archetype.CarpenterArchetype;
 import ao.model.character.archetype.ClericArchetype;
 import ao.model.character.archetype.DruidArchetype;
 import ao.model.character.archetype.FisherArchetype;
+import ao.model.character.archetype.HunterArchetype;
 import ao.model.character.archetype.LumberjackArchetype;
 import ao.model.character.archetype.MageArchetype;
 import ao.model.character.archetype.MinerArchetype;
+import ao.model.character.archetype.PaladinArchetype;
 import ao.model.character.archetype.PirateArchetype;
+import ao.model.character.archetype.ThiefArchetype;
+import ao.model.character.archetype.WarriorArchetype;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
@@ -53,18 +61,33 @@ public class ArchetypeModule extends AbstractModule {
 	}
 	
 	@Provides
-	MageArchetype provideMageArchetype() throws Exception {
-		return (MageArchetype) getArchetype(MageArchetype.class);
+	AssasinArchetype provideAssasinArchetype() throws Exception {
+		return (AssasinArchetype) getArchetype(AssasinArchetype.class);
 	}
 	
 	@Provides
-	ClericArchetype provideClericArchetype() throws Exception {
-		return (ClericArchetype) getArchetype(ClericArchetype.class);
+	BanditArchetype provideBanditArchetype() throws Exception {
+		return (BanditArchetype) getArchetype(BanditArchetype.class);
 	}
 	
 	@Provides
 	BardArchetype provideBardArchetype() throws Exception {
 		return (BardArchetype) getArchetype(BardArchetype.class);
+	}
+	
+	@Provides
+	BlacksmithArchetype provideBlacksmithArchetype() throws Exception {
+		return (BlacksmithArchetype) getArchetype(BlacksmithArchetype.class);
+	}
+	
+	@Provides
+	CarpenterArchetype provideCarpenterArchetype() throws Exception {
+		return (CarpenterArchetype) getArchetype(CarpenterArchetype.class);
+	}
+	
+	@Provides
+	ClericArchetype provideClericArchetype() throws Exception {
+		return (ClericArchetype) getArchetype(ClericArchetype.class);
 	}
 	
 	@Provides
@@ -78,17 +101,42 @@ public class ArchetypeModule extends AbstractModule {
 	}
 	
 	@Provides
+	HunterArchetype provideHunterArchetype() throws Exception {
+		return (HunterArchetype) getArchetype(HunterArchetype.class);
+	}
+	
+	@Provides
 	LumberjackArchetype provideLumberjackArchetype() throws Exception {
 		return (LumberjackArchetype) getArchetype(LumberjackArchetype.class);
+	}
+	
+	@Provides
+	MageArchetype provideMageArchetype() throws Exception {
+		return (MageArchetype) getArchetype(MageArchetype.class);
 	}
 	
 	@Provides
 	MinerArchetype provideMinerArchetype() throws Exception {
 		return (MinerArchetype) getArchetype(MinerArchetype.class);
 	}
-
+	
+	@Provides
+	PaladinArchetype providePaladinArchetype() throws Exception {
+		return (PaladinArchetype) getArchetype(PaladinArchetype.class);
+	}
+	
 	@Provides
 	PirateArchetype providePirateArchetype() throws Exception {
 		return (PirateArchetype) getArchetype(PirateArchetype.class);
+	}
+	
+	@Provides
+	ThiefArchetype provideThiefArchetype() throws Exception {
+		return (ThiefArchetype) getArchetype(ThiefArchetype.class);
+	}
+
+	@Provides
+	WarriorArchetype provideWarriorArchetype() throws Exception {
+		return (WarriorArchetype) getArchetype(WarriorArchetype.class);
 	}
 }
