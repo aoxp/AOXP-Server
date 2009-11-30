fa2add8ff5e72e0787b9fe4a33581a542d490b4c
Marco Vanotti <marcovanotti15@gmail.com>
2009-11-30 22:01:27 +0000

    Agregado el header de la licencia GPLv3 a toedos los archivos fuente.
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@118 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/main/ao/model/user/LoggedUser.java
+++ AO-XP Server/src/main/ao/model/user/LoggedUser.java
@@ -1,3 +1,21 @@
+/*
+    AO-XP Server (XP stands for Cross Platform) is a Java implementation of Argentum Online's server 
+    Copyright (C) 2009 Juan Martín Sotuyo Dodero. <juansotuyo@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package ao.model.user;
 
 import ao.model.character.Character;
@@ -30,8 +48,9 @@ public class LoggedUser implements User, UserCharacter {
 	private Gender gender;
 	private Archetype archetype;
 
-	
-	/* UserFlags */
+	/*
+	 * UserFlags
+	 */
 	private Boolean poisoned = false;
 	private Boolean paralyzed = false;
 	private Boolean immobilized = false;
@@ -40,6 +59,9 @@ public class LoggedUser implements User, UserCharacter {
 	private Boolean dumbed = false;
 	private Boolean hidden = false;
 	
+	/*
+	 * UserStats
+	 */
 	private int maxMana;
 	private int maxHitPoints;
 	private int mana;
