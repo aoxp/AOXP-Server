26917aea76a10faf503bf256fa47a8e178d7dc26
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2010-02-18 14:17:23 +0000

    Armaduras. Sin mucha ciencia.
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@188 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

---
+++ AO-XP Server/src/main/ao/model/worldobject/Armor.java
@@ -0,0 +1,74 @@
+/*
+    AO-XP Server (XP stands for Cross Platform) is a Java implementation of Argentum Online's server 
+    Copyright (C) 2009 Juan Martín Sotuyo Dodero. <juansotuyo@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ao.model.worldobject;
+
+import java.util.List;
+
+import ao.model.character.Character;
+import ao.model.character.archetype.UserArchetype;
+
+/**
+ * An armor.
+ */
+public class Armor extends AbstractDefensiveItem {
+
+	/**
+	 * Creates a new Armor instance.
+	 * @param id The id of the item.
+	 * @param name The name of the item.
+	 * @param amount The item's amount.
+	 * @param tradeable True if it's tradeable, false otherwise.
+	 * @param graphic The graphic for the item.
+	 * @param value The item's value.
+	 * @param usageDifficulty The item's usage difficulty.
+	 * @param manufactureDifficulty The item's manufacture difficulty.
+	 * @param forbiddenArchetypes List of UserArchetypes not allowed to use this item.
+	 * @param newbie Whether the item is newbie or nor.
+	 * @param equippedGraphic The index of the graphic when equipped.
+	 * @param minDef The minimum defense granted by this boat.
+	 * @param maxDef The maximum defense granted by this boat.
+	 */
+	public Armor(int id, String name, int amount, boolean tradeable,
+			int graphic, int value, int usageDifficulty,
+			int manufactureDifficulty, List<UserArchetype> forbiddenArchetypes,
+			boolean newbie, int equippedGraphic, int minDef, int maxDef) {
+		super(id, name, amount, tradeable, graphic, value, usageDifficulty,
+				manufactureDifficulty, forbiddenArchetypes, newbie, equippedGraphic,
+				minDef, maxDef);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see ao.model.worldobject.AbstractItem#clone()
+	 */
+	@Override
+	public Item clone() {
+		return new Armor(id, name, amount, tradeable, graphic, value, usageDifficulty, manufactureDifficulty, forbiddenArchetypes, newbie, equippedGraphic, minDef, maxDef);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see ao.model.worldobject.Item#use(ao.model.character.Character)
+	 */
+	@Override
+	public void use(Character character) {
+		// Armors can't be used, just equipped
+	}
+
+}
---
+++ AO-XP Server/src/test/ao/model/worldobject/ArmorTest.java
@@ -0,0 +1,114 @@
+/*
+    AO-XP Server (XP stands for Cross Platform) is a Java implementation of Argentum Online's server 
+    Copyright (C) 2009 Juan Martín Sotuyo Dodero. <juansotuyo@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ao.model.worldobject;
+
+import static org.junit.Assert.*;
+
+import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ao.model.character.Character;
+
+public class ArmorTest extends AbstractDefensiveItemTest {
+
+	private static final int MIN_DEF = 1;
+	private static final int MAX_DEF = 5;
+	
+	private Armor armor1;
+	private Armor armor2;
+	
+	@Before
+	public void setUp() throws Exception {
+		armor1 = new Armor(1, "Leather Armor", 5, true, 1, 1, 0, 0, null, false, 1, MIN_DEF, MAX_DEF);
+		armor2 = new Armor(1, "Leather Armor", 1, true, 1, 1, 0, 0, null, false, 1, MAX_DEF, MAX_DEF);
+		
+		item = armor1;
+		itemGraphic = 1;
+		itemId = 1;
+		itemIsTradeable = true;
+		itemManufactureDifficulty = 0;
+		itemUsageDifficulty = 0;
+		itemName = "Leather Armor";
+		itemValue = 1;
+		itemNewbie = false;
+		itemEquipped = false;
+		itemEquippedGraphic = 1;
+		itemMinDef = MIN_DEF;
+		itemMaxDef = MAX_DEF;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testClone() {
+		Armor clone = (Armor) armor1.clone();
+		
+		// Make sure all fields match
+		assertEquals(armor1.amount, clone.amount);
+		assertEquals(armor1.forbiddenArchetypes, clone.forbiddenArchetypes);
+		assertEquals(armor1.graphic, clone.graphic);
+		assertEquals(armor1.id, clone.id);
+		assertEquals(armor1.manufactureDifficulty, clone.manufactureDifficulty);
+		assertEquals(armor1.maxDef, clone.maxDef);
+		assertEquals(armor1.minDef, clone.minDef);
+		assertEquals(armor1.name, clone.name);
+		assertEquals(armor1.tradeable, clone.tradeable);
+		assertEquals(armor1.usageDifficulty, clone.usageDifficulty);
+		assertEquals(armor1.value, clone.value);
+		
+		// Make sure the object itself is different
+		assertFalse(armor1 == clone);
+		
+		
+		clone = (Armor) armor2.clone();
+		
+		// Make sure all fields match
+		assertEquals(armor2.amount, clone.amount);
+		assertEquals(armor2.forbiddenArchetypes, clone.forbiddenArchetypes);
+		assertEquals(armor2.graphic, clone.graphic);
+		assertEquals(armor2.id, clone.id);
+		assertEquals(armor2.manufactureDifficulty, clone.manufactureDifficulty);
+		assertEquals(armor2.maxDef, clone.maxDef);
+		assertEquals(armor2.minDef, clone.minDef);
+		assertEquals(armor2.name, clone.name);
+		assertEquals(armor2.tradeable, clone.tradeable);
+		assertEquals(armor2.usageDifficulty, clone.usageDifficulty);
+		assertEquals(armor2.value, clone.value);
+		
+		// Make sure the object itself is different
+		assertFalse(armor2 == clone);
+	}
+
+	@Test
+	public void testUse() {
+		Character character = EasyMock.createMock(Character.class);
+		EasyMock.replay(character);
+		
+		// nothing should happen
+		armor1.use(character);
+		armor2.use(character);
+		
+		EasyMock.verify(character);
+	}
+
+}
