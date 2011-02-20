f405faff8237126bbb73cb9b2363a3372756173c
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2011-02-20 20:20:06 +0000

    Better test, prevent one failure from affecting other tests.
    git-svn-id: https://aoxp.googlecode.com/svn/trunk@318 4a1aa3fa-7d1d-11de-9afa-c7a50e5ca1b3

--- AO-XP Server/src/test/java/com/ao/data/dao/ini/UserDAOIniTest.java
+++ AO-XP Server/src/test/java/com/ao/data/dao/ini/UserDAOIniTest.java
@@ -1,5 +1,5 @@
 /*
-    AO-XP Server (XP stands for Cross Platform) is a Java implementation of Argentum Online's server 
+    AO-XP Server (XP stands for Cross Platform) is a Java implementation of Argentum Online's server
     Copyright (C) 2009 Juan Martín Sotuyo Dodero. <juansotuyo@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
@@ -26,6 +26,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 
 import org.easymock.classextension.EasyMock;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -41,64 +42,71 @@ import com.ao.model.user.Account;
 public class UserDAOIniTest {
 
 	private UserDAOIni dao;
-	
+
 	private static final String CHARACTER_NICK = "test";
 	private static final String NEW_CHARACTER_NICK = "newchartest";
 	private static final String CHARACTER_MAIL = "test@test.com";
 	private static final String CHARACTER_PASSWORD = "testpass";
 	private static final String CHARFILES_PATH = "src/test/resources/charfiles/";
-	
+
 	@Before
 	public void setUp() throws Exception {
 		// Disable escaping for Ini4j
 		System.setProperty("org.ini4j.config.escape", "false");
-		
+
 		dao = new UserDAOIni(CHARFILES_PATH);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// Be really sure the file is not there before the next test
+		File file = new File(dao.getCharFilePath(NEW_CHARACTER_NICK));
+		file.delete();
 	}
 
 	@Test
 	public void testRetrieve() throws DAOException {
 		Account acc = dao.retrieve(CHARACTER_NICK);
-		
+
 		assertNotNull(acc);
-		
-		// Enssure we get the requested character and not another one.
+
+		// Ensure we get the requested character and not another one.
 		assertEquals(acc.getName(), CHARACTER_NICK);
 	}
 
 	@Test
 	public void testCreateAccount() throws DAOException {
 		Account acc = dao.create(NEW_CHARACTER_NICK, CHARACTER_PASSWORD, CHARACTER_MAIL);
-		
+
 		assertEquals(acc.getName(), NEW_CHARACTER_NICK);
 		assertEquals(acc.getMail(), CHARACTER_MAIL);
-		
+
 		assertTrue(acc.authenticate(CHARACTER_PASSWORD));
-		
+
 		File file = new File(dao.getCharFilePath(NEW_CHARACTER_NICK));
-		
+
 		assertTrue(file.exists());
-		
+
 		// Don't leave the file there!
 		file.delete();
 	}
-	
+
 	@Test
 	public void testDelete() throws DAOException {
 		dao.create(NEW_CHARACTER_NICK, CHARACTER_PASSWORD, CHARACTER_MAIL);
 		File file = new File(dao.getCharFilePath(NEW_CHARACTER_NICK));
-		
+
 		assertTrue(file.exists());
-		
+
 		dao.delete(NEW_CHARACTER_NICK);
-		
+
 		assertFalse(file.exists());
 	}
-	
+
 	@Test
 	public void testCreateCharacter() throws DAOException {
 		byte[] skills = new byte[Skill.AMOUNT];
-		
+
 		for (int i = 0; i < Skill.AMOUNT; i++) {
 			if (i == 1) {
 				skills[i] = 10;
@@ -106,30 +114,30 @@ public class UserDAOIniTest {
 				skills[i] = 0;
 			}
 		}
-		
+
 		City city = EasyMock.createMock(City.class);
 		EasyMock.expect(city.getMap()).andReturn(1).anyTimes();
 		EasyMock.expect(city.getX()).andReturn((byte) 10).anyTimes();
 		EasyMock.expect(city.getY()).andReturn((byte) 20).anyTimes();
 		EasyMock.replay(city);
-		
+
 		// TODO: Use constants!!
 		UserCharacter chara = dao.create(NEW_CHARACTER_NICK, Race.HUMAN, Gender.FEMALE,
-				UserArchetype.ASSASIN, (int) 75, city, (byte) 18, (byte) 18,
-				(byte) 18, (byte) 18, (byte) 18, (int) 10, (int)1);
-		
+				UserArchetype.ASSASIN, 75, city, (byte) 18, (byte) 18,
+				(byte) 18, (byte) 18, (byte) 18, 10, 1);
+
 		File file = new File(dao.getCharFilePath(NEW_CHARACTER_NICK));
-		
+
 		assertTrue(file.exists());
-		
+
 		assertEquals(chara.getName(), NEW_CHARACTER_NICK);
 		assertEquals(chara.getArchetype(), UserArchetype.ASSASIN.getArchetype());
 		assertEquals(chara.getGender(), Gender.FEMALE);
 		assertEquals(chara.getRace(), Race.HUMAN);
 		assertEquals(chara.getLevel(), (byte) 1);
-		
+
 		// TODO: To be continued... :P
-		
+
 		file.delete();
 	}
 }
